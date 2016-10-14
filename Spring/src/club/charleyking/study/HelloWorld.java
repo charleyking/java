da8217dba6ec9f9c5770c321ef6452d16070137a
charleyking <wangjiahao007@gmail.com>
2016-10-14 23:50:37 +0800

    spring practice

---
+++ Spring/src/club/charleyking/study/HelloWorld.java
@@ -0,0 +1,12 @@
+package club.charleyking.study;
+
+public class HelloWorld {
+	private String message;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public void getMessage() {
+		System.out.println("your message is " + message);
+	}
+}
