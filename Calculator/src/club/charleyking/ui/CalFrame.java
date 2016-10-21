5846e2a267fd275b50c6e67064ab5034e1e36ece
charleyking <wangjiahao007@gmail.com>
2016-10-21 17:16:52 +0800

    swing work

--- Calculator/src/club/charleyking/ui/CalFrame.java
+++ Calculator/src/club/charleyking/ui/CalFrame.java
@@ -2,8 +2,10 @@ package club.charleyking.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,21 +13,27 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CalFrame extends JFrame {
-	private String[] buttonName = {"1","2","3","4","5","6","7","8","9","+","-","*","/","="};
-	private JLabel label;
+	private String[] buttonName = {"1","2","3","4","5","6","7","8","9","0","+","-","*","/","="};
+	private JTextField textField;
 	private JPanel panel;
 	
 	public CalFrame() {
 		panel = new JPanel();
-		label = new JLabel("hello");
-		label.setBounds(0, 0, 100, 200);
-		panel.setLayout(new GridLayout(4,4));
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(200, 40));
+		textField.setHorizontalAlignment(JTextField.RIGHT);
+		panel.setLayout(new GridLayout(5,4,20,20));
+		panel.setBorder(BorderFactory.createLineBorder(Color.pink));
 		for (int i=0; i<buttonName.length; i++) {
 			panel.add(new JButton(buttonName[i]));
 		}
 		this.setLayout(new BorderLayout());
 		this.getContentPane().add(panel, BorderLayout.CENTER);
-		this.getContentPane().add(label, BorderLayout.NORTH);
+		this.getContentPane().add(textField, BorderLayout.NORTH);
+		this.init();
+	}
+	
+	public void init() {
 		this.setSize(500, 500);
 		this.setTitle("calculator");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
--- Test/src/club/charleyking/string/NoString.java
+++
@@ -1,8 +0,0 @@
-package club.charleyking.string;
-
-public class NoString {
-	public static void main(String[] args) {
-		String string = "hela";
-		System.out.println(string);
-	}
-}
