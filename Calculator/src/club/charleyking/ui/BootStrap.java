184bcb52a9f9056c69ce215f536c4e51ea17b3fb
charleyking <wangjiahao007@gmail.com>
2016-10-24 17:55:04 +0800

    calculator

---
+++ Calculator/src/club/charleyking/ui/BootStrap.java
@@ -0,0 +1,8 @@
+package club.charleyking.ui;
+
+public class BootStrap {
+	public static CalFrame frame;
+	public static void main(String[] args) {
+		frame = new CalFrame();
+	}
+}
--- Calculator/src/club/charleyking/ui/CalFrame.java
+++ Calculator/src/club/charleyking/ui/CalFrame.java
@@ -1,36 +1,96 @@
 package club.charleyking.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import club.charleyking.listener.ButtonListener;
+import club.charleyking.listener.OperationListener;
 
 public class CalFrame extends JFrame {
-	private String[] buttonName = {"1","2","3","4","5","6","7","8","9","0","+","-","*","/","="};
-	private JTextField textField;
-	private JPanel panel;
+	public String[] buttonName = {"0","1","2","3","4","5","6","7","8","9","+","-","*","/","="};
+	public JButton[] buttons;
+	public JTextField textField;
+	public JPanel panel, textPanel;
+	public Border emptyEdge, blackline;
+	public JButton button, buttonAdd, buttonSub, buttonTime, buttonDivide, buttonEqual;
+	public ActionListener listener;
+	public int operation, firstNumber, secondNumber, result;
 	
 	public CalFrame() {
 		panel = new JPanel();
+		//textPanel = new JPanel();
 		textField = new JTextField();
+		buttons = new JButton[10];
+		blackline = BorderFactory.createLineBorder(Color.black);
+		emptyEdge = BorderFactory.createEmptyBorder(40,10,10,10);
 		textField.setPreferredSize(new Dimension(200, 40));
 		textField.setHorizontalAlignment(JTextField.RIGHT);
+		textField.setEditable(false);
+		//textPanel.add(textField);
+		textField.setBorder(blackline);
 		panel.setLayout(new GridLayout(5,4,20,20));
-		panel.setBorder(BorderFactory.createLineBorder(Color.pink));
-		for (int i=0; i<buttonName.length; i++) {
-			panel.add(new JButton(buttonName[i]));
+		panel.setBorder(emptyEdge);
+		for (int i=0; i<10; i++) {
+			addButton(panel, String.valueOf(i));
 		}
-		this.setLayout(new BorderLayout());
+		addOperationButton(panel, "+", 1);
+		addOperationButton(panel, "-", 2);
+		addOperationButton(panel, "*", 3);
+		addOperationButton(panel, "/", 4);
+		buttonEqual = new JButton("=");
+		buttonEqual.setActionCommand("=");
+		buttonEqual.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				secondNumber = Integer.parseInt(textField.getText());
+				textField.setText("=");
+				if (operation == 1) {
+					result = firstNumber + secondNumber;
+				} else if (operation == 2) {
+					result = firstNumber - secondNumber;
+				} else if (operation == 3) {
+					result = firstNumber * secondNumber;
+				} else if (operation == 4) {
+					result = firstNumber / secondNumber;
+				}
+				textField.setText(String.valueOf(result));
+			}
+		});
+		panel.add(buttonEqual);
+		//this.setLayout(new BorderLayout());
 		this.getContentPane().add(panel, BorderLayout.CENTER);
 		this.getContentPane().add(textField, BorderLayout.NORTH);
 		this.init();
+	}
+	
+	public void addOperationButton(JPanel panel, String name, int operation) {
+		button = new JButton(name);
+		listener = new OperationListener(this, operation);
+		button.setActionCommand(name);
+		button.addActionListener(listener);
+		panel.add(button);
+	}
+	
+	public void addButton(JPanel panel, String name) {
+		button = new JButton(name);
+		listener = new ButtonListener(this);
+		button.setActionCommand(name);
+		button.addActionListener(listener);
+		panel.add(button);
 	}
 	
 	public void init() {
@@ -40,7 +100,4 @@ public class CalFrame extends JFrame {
 		this.setVisible(true);
 	}
 	
-	public static void main(String[] args) {
-		CalFrame calculator = new CalFrame();
-	}
 }
